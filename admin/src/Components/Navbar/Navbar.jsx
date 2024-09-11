17e449e784538c24ae39b677975bdf2064b03782
Bachbeos <trinhbachtt@gmail.com>
2024-09-11 22:13:26 +0700

    Done navbar

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import './Navbar.css'
+import navlogo from '../../Assets/Admin_Assets/nav-logo.svg'
+import navProfile from '../../Assets/Admin_Assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className="navbar">
-
+      <img className='nav-logo' src={navlogo} alt="" />
+      <img className='nav-profile' src={navProfile} alt="" />
     </div>
   )
 }

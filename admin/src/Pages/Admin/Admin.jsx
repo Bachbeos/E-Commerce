15d8f58d49a8e67576aeec9414b03c36705eb713
Bachbeos <trinhbachtt@gmail.com>
2024-09-11 23:27:40 +0700

    Done Sidebar

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,9 +1,10 @@
+import Sidebar from '../../Components/Sidebar/Sidebar'
 import './Admin.css'
 
 const Admin = () => {
   return (
     <div className='admin'>
-
+      <Sidebar/>
     </div>
   )
 }
